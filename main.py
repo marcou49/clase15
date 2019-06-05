0f8e33c3d2946122fd81476d29da47f23ce24308
marcou49 <richard.casares@gmail.com>
2019-06-05 11:46:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/portfolio/google/")
+def google():
+    return render_template("google.html")
+
+@app.route("/portfolio/peluqueria/")
+def peluqueria():
+    return render_template("peluqueria.html")
+
+@app.route("/portfolio/facebook/")
+def facebook():
+    return render_template("facebook.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run()
+
